a127982fb69258e343f9aa562aa960bd02e6a331
Matt Biggin <matthew.biggin@gmail.com>
2014-09-22 20:42:22 +0100

    Added basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    watch: {
+      unit: {
+        files: [
+          "index.js", "lib/*.js", "lib/consecution/*.js",
+          "spec/**/*.js"
+        ],
+        tasks: ["jasmine_node"]
+      },
+
+      acceptance: {
+        files: [
+          "index.js", "lib/*.js", "lib/consecution/*.js",
+          "spec/**/*.js", "features/*.feature", "features/step_definitions/*.js"
+        ],
+        tasks: ["cucumberjs"]
+      }
+    },
+
+    jasmine_node: {
+      options: {
+        forceExit: true,
+        match: ".",
+        matchall: false,
+        extensions: "js",
+        specNameMatcher: "spec",
+        jUnit: {
+          report: true,
+          savePath: "./build/reports/jasmine/",
+          useDotNotation: true,
+          consolidate: true
+        }
+      },
+      all: ["spec/"]
+    },
+
+    cucumberjs: {
+      src: 'features',
+      options: {
+        steps: "features/step_definitions"
+      }
+    }
+  });
+
+  require("matchdep").filterDev(["grunt-*", "!grunt-cli"]).forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask("default", ["unit"]);
+  grunt.registerTask("test", ["jasmine_node", "cucumberjs"]);
+  grunt.registerTask("unit", ["watch:unit"]);
+  grunt.registerTask("acceptance", ["watch:acceptance"]);
+};
